3afb7d6d167822ca8031b57cb3b0c98f446f49e9
JCCotrina <juan.cotrina.m@uni.pe>
2021-02-21 00:04:49 -0500

    upload last version

--- albuniFront/src/index.js
+++ albuniFront/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import SignUpConsumidor from './SignUpConsumidor';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <SignUpConsumidor/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
